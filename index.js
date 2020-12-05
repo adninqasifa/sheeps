2c74c00b6beac2e0bee95f1435e39e797ded1667
Adnin Qasifa <45332823+adninqasifa@users.noreply.github.com>
2020-12-05 17:23:25 +0700

    Tugas Mock Tech Interview Exercise

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ function Test(fun, result) {
     console.log(fun === result)
 }
 
-console.log();
-
 // Test assertions
 Test(warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep"]), "Oi! Sheep number 1! You are about to be eaten by a wolf!");
 Test(warnTheSheep(["sheep", "sheep", "wolf"]), "Pls go away and stop eating my sheep");
