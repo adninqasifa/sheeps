1585e8947e521023b5c4e68a9cc4087c7dd748cb
Adnin Qasifa <adninqasifa@gmail.com>
2020-12-12 17:32:13 +0700

    Delete

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-'''
-Program Name : Hello World Python
-Name		     : Adnin Qasifa
-'''
-# Program Hello World with Python
-# Compile and run Ctrl+Shift+B
-
-print("Hello World with Python")
-print("by Adnin Qasifa")
